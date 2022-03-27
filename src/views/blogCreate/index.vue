3cfc2baa98dce2b57d1a6235890047e87e8dc9cc
Lokki-ki-ki <74096861+Lokki-ki-ki@users.noreply.github.com>
2022-03-27 12:56:37 +0800

    Update index.vue

--- src/views/blogCreate/index.vue
+++ src/views/blogCreate/index.vue
@@ -1,11 +1,19 @@
 <template>
   <!-- <editor-content class="eidtor" :editor="editor" /> -->
-  <div>
+  <div class="editone">
     <label>Your Title</label>
     <input placeholder="Enter your title" v-model="title" />
   </div>
-  <QuillEditor ref="qeditor" class="editor" theme="snow" toolbar="full" @ready="onEditorReady($event)" />
-  <button class="editbutton" v-on:click="upload()">Upload</button>
+  <div class="editcontainer">
+    <QuillEditor
+      ref="qeditor"
+      class="editor"
+      theme="snow"
+      toolbar="Full"
+      @ready="onEditorReady($event)"
+    />
+    <button class="editbutton" v-on:click="upload()">Upload</button>
+  </div>
 </template>
 
 <script>
@@ -25,7 +33,7 @@ export default {
       editor: null,
       user: this.$store.user,
       title: null,
-      isEditing: true
+      isEditing: true,
     };
   },
 
@@ -33,15 +41,16 @@ export default {
 
   computed: {
     qeditor() {
-      return this.$refs.qeditor.getQuill()
-    }
+      return this.$refs.qeditor.getQuill();
+    },
   },
 
   methods: {
     onEditorReady() {
-      console.log(this.qeditor)
+      console.log(this.qeditor);
       // this.qeditor.setText("Please enter enter your blogs here...")
-      this.qeditor.root.dataset.placeholder = "Please enter enter your blogs here..."
+      this.qeditor.root.dataset.placeholder =
+        "Please enter enter your blogs here...";
     },
 
     async upload() {
@@ -49,21 +58,22 @@ export default {
         const auth = getAuth();
         const fbuser = auth.currentUser.email;
         if (!this.title) {
-          alert("Please enter the title!")
-          return
+          alert("Please enter the title!");
+          return;
         }
 
         await setDoc(doc(db, String(fbuser), this.title), {
           Name: this.title,
           Content: this.qeditor.root.innerHTML,
         });
-        alert("Upload successfully!")
+        alert("Upload successfully!");
         this.title = null;
-        this.qeditor.setText("")
-        this.qeditor.root.dataset.placeholder = "Please enter enter your blogs here..."
+        this.qeditor.setText("");
+        this.qeditor.root.dataset.placeholder =
+          "Please enter enter your blogs here...";
       } catch (e) {
         console.error("Error adding document: ", e);
-        alert("Sth went wrong")
+        alert("Sth went wrong");
       }
     },
   },
@@ -71,14 +81,20 @@ export default {
 </script>
 
 <style scoped>
-.editor {
+.editcontainer {
   width: 80%;
-  height: 100px;
+  margin: 1% 10%;
+  height: 500px;
   border-style: solid;
   border-width: 2px;
-  border-color: blue;
-  margin-left: 20px;
+  border-color: black;
   padding-bottom: 40px;
+  align-items: center;
 }
+
+.editbutton {
+  margin-top: 20px;
+}
+
 </style>>
 

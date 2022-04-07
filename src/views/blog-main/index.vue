63208108a568b7c8c1b604bf42232abcac52fd68
Lokki-ki-ki <74096861+Lokki-ki-ki@users.noreply.github.com>
2022-04-07 21:32:36 +0800

    0407-indivAttraction comments update

--- src/views/blog-main/index.vue
+++ src/views/blog-main/index.vue
@@ -83,6 +83,7 @@ const previous = () => {
     return
   }
   currImg.value = list[--index.value]
+
 }
 </script>
 
--- src/views/indivAttractionPager/index.vue
+++ src/views/indivAttractionPager/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="indiv-attraction-pager">
     <div class="top-bg">
-      <div class="Victoria">{{attraction.name}}</div>
+      <div class="Victoria">{{ attraction.name }}</div>
       <div class="Webinar">
         A Free Webinar on How to Unleash Your Inner Entrepreneur.
       </div>
@@ -11,47 +11,76 @@
         <!-- <span @click="jumpPage('index')">destination</span> > <span @click="jumpPage('trip-plan-list')">Hongkong</span> > <span>attraction</span> -->
       </div>
       <div class="image-main">
-        <img src="../../assets/image/4bc7041ef2e94013a1802911c4c70070.jpeg" alt="">
+        <img
+          src="../../assets/image/4bc7041ef2e94013a1802911c4c70070.jpeg"
+          alt=""
+        />
       </div>
       <div class="desc-main">
         <h2>
-          Victoria Peak (Tai Ping Shan in Chinese), also known as Mount Austin, or locally as The Peak. It is the highest hill on Hong Kong Island with an elevation of 552m. With around 7 million visitors per year, the Peak is a major tourist attraction of Hong Kong. It has views of the city and its waterfront.
+          Victoria Peak (Tai Ping Shan in Chinese), also known as Mount Austin,
+          or locally as The Peak. It is the highest hill on Hong Kong Island
+          with an elevation of 552m. With around 7 million visitors per year,
+          the Peak is a major tourist attraction of Hong Kong. It has views of
+          the city and its waterfront.
         </h2>
-        <div class="Ticket">
-          Buy The Ticket
-        </div>
+        <div class="Ticket">Buy The Ticket</div>
       </div>
     </div>
     <div class="comments-block">
       <div class="title-block">
-        <h2>
-          Users' Comments on this City
-        </h2>
-        <div class="More">
-          Read More >
-        </div>
+        <h2>Users' Comments on this City</h2>
+        <div class="More">Read More ></div>
       </div>
-        在这里放comments的展示
+      <div class="commit-list">
+        <Comments
+          :comments="comment"
+          v-for="comment in comments"
+          :key="comment.date"
+        />
+      </div>
     </div>
     <div>
       <div class="title-block">
-        <h2>
-          Add your Comment
-        </h2>
+        <h2>Add your Comment</h2>
       </div>
       <div class="textAre-block">
-        <textarea placeholder="How can we do better next time?" name="" id=""></textarea>
+        <textarea
+          placeholder="How can we do better next time?"
+          v-model="commentarea"
+          required="true"
+        ></textarea>
+        <button class="btn-comments" @click="submitCom">Submit</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { doc, getDoc } from "firebase/firestore";
+import {
+  doc,
+  getDoc,
+  addDoc,
+  collection,
+  Timestamp,
+  query,
+  orderBy,
+  limit,
+  getDocs,
+} from "firebase/firestore";
 import { ref } from "vue";
 import { db } from "../../firebase/index";
+import Comments from "../../components/Comments.vue";
 export default {
   props: ["id"],
+  data() {
+    return {
+      commentarea: "",
+    };
+  },
+  components: {
+    Comments,
+  },
   setup(props) {
     const attraction = ref("");
     const load = async () => {
@@ -63,147 +92,195 @@ export default {
         console.log(res.data());
       } catch (err) {
         alert(err.message);
-        console.log(err)
+        console.log(err);
       }
+    };
+    const comments = ref([]);
+    const dbRef = collection(
+      db,
+      "cities/Hong-Kong" + "/attractions/" + props.id + "/comments"
+    );
+    const loadComm = async () => {
+      const q = query(dbRef, orderBy("date", "desc"), limit(3));
+      const res = await getDocs(q);
+      comments.value = res.docs.map((doc) => {
+        // console.log(doc.data());
+        return { ...doc.data(), id: doc.id };
+      });
     };
 
     load();
+    loadComm();
     return {
       attraction,
+      comments,
     };
   },
-};
+  methods: {
+    async submitCom() {
+      if (!this.commentarea) {
+        alert("Cannot submit empty comments");
+        return;
+      }
+      try {
+        await addDoc(
+          collection(
+            db,
+            "cities/Hong-Kong" + "/attractions/" + this.id + "/comments"
+          ),
+          {
+            date: Timestamp.fromDate(new Date()),
+            username: this.$store.state.username,
+            userId: this.$store.state.user.uid,
+            content: this.commentarea,
+          }
+        );
+        this.loadComm();
+      } catch (err) {
+        console.log(err);
+      }
 
+      alert("Comments successfully!");
+      this.commentarea = "";
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
 .indiv-attraction-pager {
-  background: url(../../assets/image/bc3ec7bc825c4e6ca746c659189cea83.jpeg) no-repeat;
-    background-attachment:fixed;
-    background-size: cover;
-    padding-bottom: 30px;
-    .top-bg {
-      background: url(../../assets/image/bigimg.jpeg) no-repeat;
-      background-size: 100% 100%;
-      height: 560px;
-      width: 100%;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      .Victoria {
-        font-size: 80px;
-        letter-spacing: normal !important;
-        color: #fff;
-        text-align: center;
-        font-weight: bold;
-      }
-      .Webinar {
-        color: #fff;
-        font-weight: bold;
-        text-align: center;
-        font-size: 20px;
-        letter-spacing: normal !important;
-      }
+  background: url(../../assets/image/bc3ec7bc825c4e6ca746c659189cea83.jpeg)
+    no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  padding-bottom: 30px;
+  .top-bg {
+    background: url(../../assets/image/bigimg.jpeg) no-repeat;
+    background-size: 100% 100%;
+    height: 560px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    .Victoria {
+      font-size: 80px;
+      letter-spacing: normal !important;
+      color: #fff;
+      text-align: center;
+      font-weight: bold;
     }
-    .image-list {
-      margin: 0 auto;
-      width: 980px;
-      position: relative;
-      margin-bottom: 100px;
-      .mbx {
-        font-size: 20px;
-        padding: 10px 0;
-        span {
-          font-size: 20px;
-          cursor: pointer;
-        }
-      }
-      .image-main {
-        width: 590px;
-        height: 483px;
-        z-index: 10;
-        position: relative;
-        img {
-          height: 100%;
-          width: 100%;
-        }
-      }
-      .desc-main {
-        width: 600px;
-        height: 444px;
-        background: #fff;
-        position: absolute;
-        right: -210px;
-        bottom: -80px;
-        padding: 20px;
-        padding-top: 80px;
-        h2 {
-          font-size: 16px;
-          line-height: 1.8;
-        }
-        .Ticket {
-          width: 190px;
-          height: 40px;
-          margin-top: 90px;
-          margin-left: 110px;
-          background: #3c72d3;
-          line-height: 40px;
-          text-align: center;
-          color: #fff;
-          cursor: pointer;
-          &:hover {
-            background: #fff;
-            color: #3c72d3;
-          }
-        }
-      }
+    .Webinar {
+      color: #fff;
+      font-weight: bold;
+      text-align: center;
+      font-size: 20px;
+      letter-spacing: normal !important;
     }
-  .title-block{
-      padding: 50px 130px;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      h2 {
-        font-size: 30px;
-      }
-      .More {
-        letter-spacing:normal !important;
+  }
+  .image-list {
+    margin: 0 auto;
+    width: 980px;
+    position: relative;
+    margin-bottom: 100px;
+    .mbx {
+      font-size: 20px;
+      padding: 10px 0;
+      span {
         font-size: 20px;
         cursor: pointer;
+      }
+    }
+    .image-main {
+      width: 590px;
+      height: 483px;
+      z-index: 10;
+      position: relative;
+      img {
+        height: 100%;
+        width: 100%;
+      }
+    }
+    .desc-main {
+      width: 600px;
+      height: 444px;
+      background: #fff;
+      position: absolute;
+      right: -210px;
+      bottom: -80px;
+      padding: 20px;
+      padding-top: 80px;
+      h2 {
+        font-size: 16px;
+        line-height: 1.8;
+      }
+      .Ticket {
+        width: 190px;
+        height: 40px;
+        margin-top: 90px;
+        margin-left: 110px;
+        background: #3c72d3;
+        line-height: 40px;
+        text-align: center;
+        color: #fff;
+        cursor: pointer;
         &:hover {
-          border: 0px solid #000000;
-          border-bottom: 1px solid #000000;
-          background: rgba(255, 255, 255, 0);
-          letter-spacing: 0.2em !important;
+          background: #fff;
+          color: #3c72d3;
         }
       }
     }
-
-    .textAre-block {
-      height: 265px;
-      width: 980px;
-      border: 2px solid rgb(145,145,145;);
-      margin: 0 auto;
-      textarea {
-        padding: 10px;
-        height: 100%;
-        width: 100%;
-        border: none;
-        /*去除阴影*/
-        box-shadow:none;
-        /*聚焦input的蓝色边框*/
-        outline: none;
-        /*textarea 禁止拖拽*/
-        resize: none;
-        /*去除边框*/
-        border: none;
-        /*常用于IOS下移除原生样式*/
-        -webkit-appearance: none;
-        /*点击高亮的颜色*/
-        -webkit-tap-highlight-color:rgba(0,0,0,0);
-      }
-
+  }
+  .title-block {
+    padding: 50px 130px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    h2 {
+      font-size: 30px;
     }
+    .More {
+      letter-spacing: normal !important;
+      font-size: 20px;
+      cursor: pointer;
+      &:hover {
+        border: 0px solid #000000;
+        border-bottom: 1px solid #000000;
+        background: rgba(255, 255, 255, 0);
+        letter-spacing: 0.2em !important;
+      }
+    }
+  }
+
+  .textAre-block {
+    height: 265px;
+    width: 980px;
+    border: 2px solid rgb(145, 145, 145;);
+    margin: 0 auto;
+    textarea {
+      padding: 10px;
+      height: 100%;
+      width: 100%;
+      border: none;
+      /*去除阴影*/
+      box-shadow: none;
+      /*聚焦input的蓝色边框*/
+      outline: none;
+      /*textarea 禁止拖拽*/
+      resize: none;
+      /*去除边框*/
+      border: none;
+      /*常用于IOS下移除原生样式*/
+      -webkit-appearance: none;
+      /*点击高亮的颜色*/
+      -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    }
+  }
+}
+.commit-list {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-items: center;
 }
 </style>

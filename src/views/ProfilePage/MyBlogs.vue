a8478e56011a71a9b9f2bdccfbca3e5721a7941f
Lokki-ki-ki <74096861+Lokki-ki-ki@users.noreply.github.com>
2022-04-08 20:49:18 +0800

    0408 - fixed profile blog bug

--- src/views/ProfilePage/MyBlogs.vue
+++ src/views/ProfilePage/MyBlogs.vue
@@ -1,94 +1,96 @@
 <template>
-    <UserHeader :name="this.$store.state.username" imgpath=""/>
-    <profile-nav/>
-    <div class="container">
-        <div  v-for="blog in myblogs.slice(0,len)" :key="blog.id">
-            <Blogs :blogs="blog" />
-        </div>
+  <UserHeader :name="this.$store.state.username" imgpath="" />
+  <profile-nav />
+  <div class="container">
+    <div v-for="blog in myblogs.slice(0, len)" :key="blog.id">
+      <Blogs :blogs="blog" />
     </div>
-    <button class="readbtn" @click="len++">Read More</button> <br> <br> <br>    
+  </div>
+  <button class="readbtn" @click="len++">Read More</button> <br />
+  <br />
+  <br />
 </template>
 
 <script>
-import Blogs from '../../components/ProfileComponents/Blogs.vue'
-import ProfileNav from '../../components/ProfileComponents/ProfileNav.vue'
-import UserHeader from "../../components/ProfileComponents/UserHeader.vue"
+import Blogs from "../../components/ProfileComponents/Blogs.vue";
+import ProfileNav from "../../components/ProfileComponents/ProfileNav.vue";
+import UserHeader from "../../components/ProfileComponents/UserHeader.vue";
 import { ref } from "vue";
-import { collection, getDocs, query, where } from '@firebase/firestore';
-import { db } from '../../firebase';
-// import {useStore} from "vuex"
-// import { computed } from 'vue'
+import { collection, getDocs, query, where } from "@firebase/firestore";
+import { db } from "../../firebase";
+import { useStore } from "vuex";
+import { computed} from "vue";
+
 
 export default {
   components: { UserHeader, Blogs, ProfileNav },
-    data() {
-        return {
-            len: 3,
-        }
-    },
-    setup() {
-        const myblogs = ref([]);
-        // const store = useStore();
-        // const user = computed(() => store.state.user)
-        const load = async () => {
-            const dbRef = collection(db, "blogs");
-            const q = query(dbRef, where("authorId","==","l4BenEYfTaMPUyRlE52I7yxQbBN2"))
-            const res = await getDocs(q);
-            myblogs.value = res.docs.map(doc => {
-                // console.log(doc.data())
-                return { ...doc.data(),id:doc.id }
-            })
-        }
-        load()
-        return { myblogs}
-    }
+  data() {
+    return {
+      len: 3,
+      userid: this.$store.state.user.uid,
+    };
+  },
+  setup() {
+    const myblogs = ref([]);
 
-}
+    const load = async () => {
+      const store = useStore();
+      const uid = computed(() => store.state.user.uid);
+      const dbRef = collection(db, "blogs");
+      const q = query(dbRef, where("authorId", "==", uid.value));
+      const res = await getDocs(q);
+      myblogs.value = res.docs.map((doc) => {
+        // console.log(doc.data())
+        return { ...doc.data(), id: doc.id };
+      });
+    };
+    load();
+    return { myblogs };
+  },
+};
 </script>
 
 <style scoped>
 .container {
-    padding-left: 5vw;
-    padding-right:5vw;
-}
-
-.readbtn{
-    width: 200px;
-    height: 80px;
-    border: none;
-    outline: none;
-    background: #2f2f2f;
-    color: #fff;
-    font-size: 22px;
-    border-radius: 40px;
-    text-align: center;
-    box-shadow: 0 6px 20px -5px rgba(0,0,0,0.4);
-    position: relative;
-    overflow: hidden;
-    cursor: pointer;
+  padding-left: 5vw;
+  padding-right: 5vw;
 }
 
 .readbtn {
-	width: 150px;
-	height: 50px;
-	cursor: pointer;
-	font-size: 20px;
-	font-weight: bold;
-	color: black;
-	background: white;
-	border: 2px solid black;
-	box-shadow: 5px 5px 0 black,
-		-5px -5px 0 black,
-		-5px 5px 0 black,
-		5px -5px 0 black;
-	transition: 500ms ease-in-out;
+  width: 200px;
+  height: 80px;
+  border: none;
+  outline: none;
+  background: #2f2f2f;
+  color: #fff;
+  font-size: 22px;
+  border-radius: 40px;
+  text-align: center;
+  box-shadow: 0 6px 20px -5px rgba(0, 0, 0, 0.4);
+  position: relative;
+  overflow: hidden;
+  cursor: pointer;
+}
+
+.readbtn {
+  width: 150px;
+  height: 50px;
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: bold;
+  color: black;
+  background: white;
+  border: 2px solid black;
+  box-shadow: 5px 5px 0 black, -5px -5px 0 black, -5px 5px 0 black,
+    5px -5px 0 black;
+  transition: 500ms ease-in-out;
 }
 
 .readbtn:hover {
-	box-shadow: 20px 5px 0 black, -20px -5px 0 black;
+  box-shadow: 20px 5px 0 black, -20px -5px 0 black;
 }
 
 .readbtn:focus {
-	outline: none;
+  outline: none;
 }
 </style>

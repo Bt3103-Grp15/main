2ec1755cbfccc77da7c7f0da5da2d79cc998219a
Chu Jia-Cheng <dorazhu@outlook.com>
2022-04-08 18:05:01 +0800

    router

--- src/views/index.vue
+++ src/views/index.vue
@@ -56,7 +56,7 @@
         <div class="second">
           <div class="item">About Us</div>
           <div class="item">Owners</div>
-          <div class="item"><router-link to='/review'>Write a Review</router-link></div>
+          <router-link to='/review'>Write a Review</router-link>
         </div>
         <div class="second">
           <div class="item">Trust&Safety</div>

68554463e050422e9fde8b336813fbb4377fe3ad
Chu Jia-Cheng <dorazhu@outlook.com>
2022-04-08 17:44:08 +0800

    revie page done

--- src/views/ReviewPage/index.vue
+++ src/views/ReviewPage/index.vue
@@ -2,14 +2,24 @@
   <main class="Container">
     <div>
       <section class="form">
-        
+          <h2>Write a review to us!</h2>
+          <p>Tell us some features that you would like to see in our website </p>
+          <textarea name="review" id="review" class="textbox"></textarea>
+          <button :click="submitReview"> sumbit </button>
+          
       </section>
     </div>
   </main>
 </template>
 
 <script>
-
+export default {
+  methods: {
+    submitReview() {
+      this.$router.push({name: "SubmitReview"})
+    }
+  }
+}
 </script>
 
 <style scoped>
@@ -18,7 +28,7 @@
   padding:10vh;
   height: 60%;
   display: flex;
-  background-image: url("../../assets/login.webp");
+  background-image: url("../../assets/review.jpg");
   background-color: white;
   background-size: 100% 100%;
 }
@@ -30,10 +40,6 @@ h2 {
   font-weight: bold;
 }
 
-.login-register {
-  font-size: 13px;
-  margin-bottom: 30px
-}
 
 .form {
   padding: 20px;
@@ -42,63 +48,41 @@ h2 {
   flex-direction: column;
   justify-content: center;
   text-align: center;
+  align-items: center;
   height: 85vh;
   width:100%;
   background: rgba(255, 255, 255, 0.8);
   border-radius: 30px;
 }
 
-.form .login{
-    width:40vw;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  align-items: center;
-  text-align: center;
-
-}
 
 
-.input {
+.textbox {
   position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-  margin-bottom: 5vh;
-  width: 50%
-}
-
-.input input {
+  margin: 5vh;
+  height: 40vh;
   width: 100%;
-  border: none;
-  padding: 4px 4px 4px 30px;
-  height: 50px;
+  border:none;
+  padding: 10px;
+
+}
+
+button {
+  width: 100px;
+  height: 35px;
+  background-color: rgb(30, 26, 26);
+  color: white;
+  justify-content: center;
+  text-align: center;
+  justify-content: center;
+  border-color: white;
   border-radius: 10px;
-  font-size:15px      
+  position: relative;
 }
 
-.btn {
-  width:30%;
-  padding:10px;
-  border: 0ch;
-  border-radius: 30px;
-  
-}
-
-.icon {
-          width: 16px;
-          position: absolute;
-          left: 6px;
-}
-
-img {
-  height:70vh;
-  width:auto;
-  padding:0;
-  margin:0;
-  display: block;
-  float: right;
-}
 
 </style>
